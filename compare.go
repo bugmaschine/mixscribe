30f54fd9b58ff8fea85ebd5c739be5ccac20983f
lowikian <38097264+bugmaschine@users.noreply.github.com>
2024-09-23 23:13:51 +0200

    hopefully it will compile now?

--- compare.go
+++ compare.go
@@ -3,9 +3,6 @@ package main
 import (
 	"bytes"
 	"log"
-
-	"github.com/go-fingerprint/fingerprint"
-	chromaprint "github.com/go-fingerprint/gochroma"
 )
 
 // Function to compare two songs from []byte buffers
@@ -17,21 +14,23 @@ func compareSongs(fp1 []int32, fp2 []int32, threshold float64) bool {
 }
 
 func compareFingerprint(fp1, fp2 []int32) float64 {
-	s, _ := fingerprint.Compare(fp1, fp2)
-	return s
+	return 1 // 100% match
+	//s, _ := fingerprint.Compare(fp1, fp2)
+	//return s
 }
 
 func calculateFingerprint(data []byte, targetSampleRate int, ComparisonSnippetTime int) []int32 {
-	reader := bytes.NewReader(data)
-	fpcalc := chromaprint.New(chromaprint.AlgorithmDefault)
-	defer fpcalc.Close()
-	fprint1, _ := fpcalc.RawFingerprint( // TODO: calculate fingerprint 1 only once at the start of recording, it dosen't make any sense to fingerprint it again, but i'm to lazy for now | Lowikian
-		fingerprint.RawInfo{
-			Src:        reader,
-			Channels:   2,
-			Rate:       uint(targetSampleRate),
-			MaxSeconds: uint(ComparisonSnippetTime),
-		})
-
-	return fprint1
+	return 0
+	//reader := bytes.NewReader(data)
+	//fpcalc := chromaprint.New(chromaprint.AlgorithmDefault)
+	//defer fpcalc.Close()
+	//fprint1, _ := fpcalc.RawFingerprint( // TODO: calculate fingerprint 1 only once at the start of recording, it dosen't make any sense to fingerprint it again, but i'm to lazy for now | Lowikian
+	//	fingerprint.RawInfo{
+	//		Src:        reader,
+	//		Channels:   2,
+	//		Rate:       uint(targetSampleRate),
+	//		MaxSeconds: uint(ComparisonSnippetTime),
+	//	})
+//
+	//return fprint1
 }
